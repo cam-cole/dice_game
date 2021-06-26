380fb1041b5644750f89b265170bec56be47d985
Cameron Cole <camcole065@gmail.com>
2021-06-26 15:12:01 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+//Generate Dice Image for Player 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1)
+
+//Generate Dice Image for Player 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2)
+
+//Conditionals to determine the Winner
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
