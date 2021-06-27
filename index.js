69bc3baf82e4e7a8ce9a45968c4d05cbf17adde2
Cameron Cole <camcole065@gmail.com>
2021-06-26 23:33:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
 
+// Changes Dice Image for Player 1
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1)
 
 //Generate Dice Image for Player 2
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
+// Changes Dice Image for Player 2
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2)
 
 //Conditionals to determine the Winner
